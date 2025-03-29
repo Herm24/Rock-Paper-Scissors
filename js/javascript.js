17ed5affad1ba53c5db7b82c5b65c6ed1745cddc
Herm24 <199443307+Herm24@users.noreply.github.com>
2025-03-29 08:28:28 +0100

    playGame function

--- js/javascript.js
+++ js/javascript.js
@@ -11,23 +11,42 @@ function getHumanChoice() {
   let choice = prompt("What's your choice?").toLowerCase();
   return choice;
 }
-let humanScore = 0;
-let computerScore = 0;
+function playGame(rounds) {
+  let humanScore = 0;
+  let computerScore = 0;
 
-function playRound(humanChoice, computerChoice) {
-  humanChoice = humanChoice.toLowerCase();
+  function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
 
-  if (
-    (humanChoice === "rock" && computerChoice === "scissors") ||
-    (humanChoice === "paper" && computerChoice === "rock") ||
-    (humanChoice === "scissors" && computerChoice === "paper")
-  ) {
-    humanScore++;
-    console.log(`You win! ${humanChoice} beats ${computerChoice}`);
-  } else if (humanChoice === computerChoice) {
-    console.log("tie i guess");
+    if (
+      (humanChoice === "rock" && computerChoice === "scissors") ||
+      (humanChoice === "paper" && computerChoice === "rock") ||
+      (humanChoice === "scissors" && computerChoice === "paper")
+    ) {
+      humanScore++;
+      console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+    } else if (humanChoice === computerChoice) {
+      console.log("tie i guess");
+    } else {
+      computerScore++;
+      console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+    }
+  }
+
+  for (let i = 0; i < rounds; i++) {
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection);
+    console.log(
+      `Current score - You: ${humanScore}, Computer: ${computerScore}`
+    );
+  }
+  console.log(`Final score - You: ${humanScore}, Computer: ${computerScore}`);
+  if (humanScore > computerScore) {
+    console.log("You won");
+  } else if (computerScore > humanScore) {
+    console.log("You lost");
   } else {
-    computerScore++;
-    console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+    console.log("tie");
   }
 }
